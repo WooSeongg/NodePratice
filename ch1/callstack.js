4016cece12f99468e3736283175c87a119f5650e
WS <blb138@naver.com>
2023-03-10 21:51:23 +0900

    첫번째 실습

---
+++ ch1/callstack.js
@@ -0,0 +1,16 @@
+
+function first(){
+    console.log("첫번째");
+    seconde();
+}
+
+function seconde(){
+    console.log("두번째");
+    third();
+}
+
+function third(){
+    console.log("세번째");
+}
+
+first();
